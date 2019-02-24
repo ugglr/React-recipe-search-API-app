6f20c8f7d215467e2e2dfaba889787db957e3bdc
CarlW <carl@oskarroger.net>
2019-02-24 14:08:31 +0800

    Search input from the Form now changes the state and displays in the DOM

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import "./App.css";
 
 import Form from "./components/Form";
 
+const API_KEY = "7a4241b148f3d499670f6d2851c57c05";
+
 class App extends Component {
-  getRecipe = e => {
+  state = {
+    recipes: []
+  };
+  getRecipe = async e => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    console.log(recipeName);
+    const api_call = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`
+    );
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
   };
 
   render() {
@@ -17,6 +28,9 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
+        {this.state.recipes.map(recipe => {
+          return <p key={recipe.recipe_id}>{recipe.title}</p>;
+        })}
       </div>
     );
   }
