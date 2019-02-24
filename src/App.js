d38abea7d17bf0d655901d99c5068b3cc55a42dd
CarlW <carl@oskarroger.net>
2019-02-24 13:37:10 +0800

    New Form Component. Props from text input working front to back.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Form from "./components/Form";
+
 class App extends Component {
+  getRecipe = e => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    console.log(recipeName);
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe} />
       </div>
     );
   }
